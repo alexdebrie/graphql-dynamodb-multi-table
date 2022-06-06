ba557d458778358a8136c5f0ffbc74a54d0d6397
Alex DeBrie <alexdebrie1@gmail.com>
2022-06-06 17:15:53 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,14 +1,36 @@
-# Welcome to your CDK TypeScript project
+## GraphQL + DynamoDB -- Multi-table example
 
-This is a blank project for CDK development with TypeScript.
+This repository includes an example of building a GraphQL with DynamoDB using multiple DynamoDB tables. It is intended to pair with [this guide on using DynamoDB in a GraphQL API](TODO).
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+## Usage
 
-## Useful commands
+This application uses the [AWS CDK](https://docs.aws.amazon.com/cdk/v2/guide/home.html) to deploy a GraphQL API to [AWS AppSync](https://aws.amazon.com/appsync/). Be sure to [install and bootstrap the CDK](https://docs.aws.amazon.com/cdk/v2/guide/getting_started.html#getting_started_install) before use.
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+To deploy, clone this repository and run the following commands:
+
+```
+npm i
+cdk deploy
+```
+
+This will deploy an AppSync API and return the GraphQL root URL and an API key to access the API.
+
+You can query the API in the AWS console, or you can open the [`index.html`](./index.html) file to use a local GraphQL explorer. Before opening the file, be sure to replace `GRAPHQL_ROOT` and `API_KEY` with the received values from your deploy.
+
+## Application background
+
+This sample application builds a portion of a SaaS blog hosting platform. Users create a Site on the platform and are able to create Posts on the site. Other users can view the Posts and attach a Comment to a Post.
+
+The simplified ERD is below. This application includes the general CDK code and AppSync resolvers to demonstrate the key differences between single-table and multi-table design with GraphQL + DynamoDB.
+
+![AppSync - ERD](https://user-images.githubusercontent.com/6509926/172209448-98350f3f-7fcf-4a7e-aa64-123dd59ab4e9.svg)
+
+## Takeaways
+
+- **This example creates a DynamoDB table for each entity.** Many DynamoDB applications and resources talk about single-table design where you use a single DynamoDB table for all entities in your application. Single-table design works well for known access patterns that fetch different types of entities in a single request. However, many GraphQL APIs allow for flexible, nested access of data where single-table design is less useful.
+
+- **Multi-table design optimizes for simplicity over latency.** This example follows traditional GraphQL practices where each resolver is simple and focused on retrieving only the type requested but not any related sub-types. This means each resolver is easier to write and understand, but it can result in a waterfall of sequential queries to your database for complex GraphQL requests.
+
+- **Multi-table design allows for a more normalized design.** With DynamoDB and single-table design, you often reduce the use of primary & foreign keys to identify relationships between items. Instead, you use attributes that will be known at query time to avoid making multiple, sequential requests.
+
+  Because the GraphQL engine plans for sequential execution of resolvers, we can use normalized tables with IDs as pointers. This eases updates of attributes on parent items, such as the domain of the parent Site.
--- lib/multi-table-cdk-stack.ts
+++ lib/multi-table-cdk-stack.ts
@@ -1,16 +1,111 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { AuthorizationType, FieldLogLevel, GraphqlApi, MappingTemplate, Schema } from "@aws-cdk/aws-appsync-alpha";
+import { CfnApiKey } from 'aws-cdk-lib/aws-appsync';
 
 export class MultiTableCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const sitesTable = new Table(this, "SitesTable", {
+      partitionKey: { name: "domain", type: AttributeType.STRING},
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MultiTableCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const postsTable = new Table(this, "PostsTable", {
+      partitionKey: { name: "siteId", type: AttributeType.STRING},
+      sortKey: { name: "publishDate", type: AttributeType.STRING},
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    })
+
+    const commentsTable = new Table(this, "CommentsTable", {
+      partitionKey: { name: "postId", type: AttributeType.STRING},
+      sortKey: { name: "publishDate", type: AttributeType.STRING},
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    })
+
+    const api = new GraphqlApi(this, 'Api', {
+      name: 'DynamoDBMultiTable',
+      schema: Schema.fromAsset('lib/schema.graphql'),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: AuthorizationType.API_KEY
+        }
+      },
+      logConfig: {
+        fieldLogLevel: FieldLogLevel.ALL
+      },
+      xrayEnabled: true
+    })
+
+    const sitesDatasource = api.addDynamoDbDataSource('SitesTable', sitesTable)
+    const postsDatasource = api.addDynamoDbDataSource('PostsTable', postsTable)
+    const commentsDatasource = api.addDynamoDbDataSource('CommentsTable', commentsTable)
+
+    sitesDatasource.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'createSite',
+      requestMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Mutation.createSite.request.vtl'),
+      responseMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Mutation.createSite.response.vtl'),
+    })
+
+    sitesDatasource.createResolver({
+      typeName: 'Query',
+      fieldName: 'getSite',
+      requestMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Query.getSite.request.vtl'),
+      responseMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Query.getSite.response.vtl'),
+    })
+
+    postsDatasource.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'createPost',
+      requestMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Mutation.createPost.request.vtl'),
+      responseMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Mutation.createPost.response.vtl'),
+    })
+
+    postsDatasource.createResolver({
+      typeName: 'Query',
+      fieldName: 'getPostsForSite',
+      requestMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Query.getPostsForSite.request.vtl'),
+      responseMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Query.getPostsForSite.response.vtl'),
+    })
+
+    postsDatasource.createResolver({
+      typeName: 'Site',
+      fieldName: 'posts',
+      requestMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Site.posts.request.vtl'),
+      responseMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Site.posts.response.vtl'),
+    })
+
+    commentsDatasource.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'createComment',
+      requestMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Mutation.createComment.request.vtl'),
+      responseMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Mutation.createComment.response.vtl'),
+    })
+
+    commentsDatasource.createResolver({
+      typeName: 'Post',
+      fieldName: 'comments',
+      requestMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Post.comments.request.vtl'),
+      responseMappingTemplate: MappingTemplate.fromFile('lib/mapping-templates/Post.comments.response.vtl'),
+    })
+
+    const apiKey = new CfnApiKey(this, 'GraphQLApiKey', {
+      apiId: api.apiId
+    })
+
+    new CfnOutput(this, 'GraphQLURLOutput', {
+      value: api.graphqlUrl,
+      exportName: 'GraphQLURL'
+    })
+
+    new CfnOutput(this, 'GraphQLApiKeyOutput', {
+      value: apiKey.attrApiKey,
+      exportName: 'GraphQLApiKey'
+    })
+
+
   }
 }
